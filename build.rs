c946bc323ebfebdef1d9657fe16d77ade6714ba0
Peter Edge <edge.peterj@gmail.com>
2018-03-31 08:42:46 -0700

    renamed c directory to hapcut2

--- build.rs
+++ build.rs
@@ -7,11 +7,11 @@ fn main() {
         .flag_if_supported("-O3")
         .flag_if_supported("-D_GNU_SOURCE")
         .flag_if_supported("-Wall")
-        .file("src/c/common.c")
-        .file("src/c/fragmatrix.c")
-        .file("src/c/readinputbuffers.c")
-        .file("src/c/pointerheap.c")
-        .file("src/c/hapcut2.c")
+        .file("src/hapcut2/common.c")
+        .file("src/hapcut2/fragmatrix.c")
+        .file("src/hapcut2/readinputbuffers.c")
+        .file("src/hapcut2/pointerheap.c")
+        .file("src/hapcut2/hapcut2.c")
         .compile("hapcut2");
 
     cc::Build::new()
