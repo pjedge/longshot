db06c93926d7ea7660fb6a9a843c07c9dad5c5b5
Peter Edge <edge.peterj@gmail.com>
2018-12-05 20:47:18 -0800

    Reworked main.rs to be more readable. Abstracted some repetitive parsing code into functions in util.rs. Started work on doc comments. Fixed bug causing haplotype assignment qual to be wrong. Fixed bug causing fragments printed to debug directory to have no singleton reads.

---
+++ src/errors.rs
@@ -0,0 +1,97 @@
+
+//! errors
+//!
+//! defines some custom errors to use with error-chain
+
+error_chain!{
+    errors {
+        // BAM errors
+        BamOpenError {
+            description("Error opening BAM file.")
+        }
+        BamWriterOpenError(f: String) {
+            description("Error opening BAM file for writing")
+            display(x) -> ("{}: {}", x.description(), f)
+        }
+        BamRecordWriteError(qname: String) {
+            description("Error writing record to bam file")
+            display(x) -> ("{}: qname {}", x.description(), qname)
+        }
+        BamHeaderTargetLenAccessError {
+            description("Error accessing target len for a contig in bam header.")
+        }
+        // Indexed BAM errors
+        IndexedBamOpenError {
+            description("Error opening indexed BAM file.")
+        }
+        IndexedBamReadError {
+            description("Error reading indexed BAM file.")
+        }
+        IndexedBamFetchError {
+            description("Error fetching region from indexed BAM file.")
+        }
+        IndexedBamRecordReadError {
+            description("Error reading BAM pileup.")
+        }
+        IndexedBamPileupReadError {
+            description("Error reading BAM pileup.")
+        }
+        IndexedBamPileupQueryPositionError {
+            description("Error accessing query position for alignment in BAM pileup.")
+        }
+        // Indexed Fasta errors
+        IndexedFastaOpenError {
+            description("Error reading indexed FASTA file.")
+        }
+        IndexedFastaReadError {
+            description("Error reading indexed FASTA file.")
+        }
+        // Indexed BAM errors
+        BCFOpenError {
+            description("Error opening BCF file.")
+        }
+        BCFReadError {
+            description("Error reading BCF file.")
+        }
+        // CIGAR errors
+        // derived from Rust-htslib errors defined with quick-error... https://github.com/rust-bio/rust-htslib/blob/master/src/bam/record.rs
+        UnexpectedCigarOperation(msg: String) {
+            description("Unexpected CIGAR operation")
+            display(x) -> ("{}: {}", x.description(), msg)
+        }
+        UnsupportedCigarOperation(msg: String) {
+            description("Unsupported CIGAR operation")
+            display(x) -> ("{}: {}", x.description(), msg)
+        }
+        // Read realignment errors
+        AnchorRangeOutsideRead {
+            description("Attempted to find sequence anchors for a range completely outside of the sequence.")
+        }
+        // Gentotype priors errors
+        InvalidTransitionBase(base: String) {
+            description("Invalid base accessed from base transition hashmap")
+            display(x) -> ("{}: {}", x.description(), base)
+        }
+        InvalidHaploidGenotype(refbase: char, altbase: char) {
+            description("Invalid base accessed from base transition hashmap")
+            display(x) -> ("{}: ref {}, alt {}", x.description(), refbase, altbase)
+        }
+        GenotypeNotInGenotypePriorsError(refbase: char, g0: char, g1: char) {
+            description("Genotype not in genotype priors.")
+            display(x) -> ("{}: ref {}, genotype ({},{})", x.description(), refbase, g0, g1)
+        }
+        // File IO errors
+        FileWriteError (filename: String){
+            description("Couldn't write to file")
+            display(x) -> ("{}: {}", x.description(), filename)
+        }
+        CreateFileError (filename: String){
+            description("Couldn't create file")
+            display(x) -> ("{}: {}", x.description(), filename)
+        }
+        // File IO errors
+        NoneError {
+            description("Option was None.")
+        }
+    }
+}
