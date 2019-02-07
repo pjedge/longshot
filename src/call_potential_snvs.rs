c13fd7ae95752ca806a004c4131c733719fadb5c
Peter Edge <edge.peterj@gmail.com>
2019-02-07 14:16:44 -0800

    fixed a bug related to new fasta reading functions in rust-bio 0.25.0

--- src/call_potential_snvs.rs
+++ src/call_potential_snvs.rs
@@ -532,7 +532,10 @@ pub fn select_random_potential_snvs(
             if tid != prev_tid {
                 let mut ref_seq_u8: Vec<u8> = vec![];
                 fasta
-                    .read_all(&chrom, &mut ref_seq_u8)
+                    .fetch_all(&chrom)
+                    .chain_err(|| ErrorKind::IndexedFastaReadError)?;
+                fasta
+                    .read(&mut ref_seq_u8)
                     .chain_err(|| ErrorKind::IndexedFastaReadError)?;
                 ref_seq = dna_vec(&ref_seq_u8);
                 next_valid_pos = 0;
