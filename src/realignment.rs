80f98913b90c077248efa6984526c6855b47694c
Peter Edge <edge.peterj@gmail.com>
2019-03-21 20:38:33 -0700

    some work on concept of homopolymer squares

--- src/realignment.rs
+++ src/realignment.rs
@@ -130,21 +130,23 @@ pub fn forward_algorithm_non_numerically_stable(
     let len_diff = ((v.len() as i32) - (w.len() as i32)).abs() as usize;
     let band_width = min_band_width + len_diff;
 
-    let mut lower_prev: Vec<f64> = vec![0.0; w.len() + 1];
-    let mut middle_prev: Vec<f64> = vec![0.0; w.len() + 1];
-    let mut upper_prev: Vec<f64> = vec![0.0; w.len() + 1];
-    let mut lower_curr: Vec<f64> = vec![0.0; w.len() + 1];
-    let mut middle_curr: Vec<f64> = vec![0.0; w.len() + 1];
-    let mut upper_curr: Vec<f64> = vec![0.0; w.len() + 1];
+    let mut forward_lower: Vec<Vec<f64>> = vec![vec![0.0; w.len() + 1]; v.len()+1];
+    let mut forward_middle: Vec<Vec<f64>> = vec![vec![0.0; w.len() + 1]; v.len()+1];
+    let mut forward_upper: Vec<Vec<f64>> = vec![vec![0.0; w.len() + 1]; v.len()+1];
 
-    middle_prev[0] = 1.0;
+    let mut last_hp: Vec<Vec<(char,usize,usize)>> = vec![vec![('N',0,0); w.len() + 1]; v.len()+1];
 
-    upper_prev[1] = params.transition_probs.deletion_from_match;
+    forward_middle[0][0] = 1.0;
+
+    forward_upper[0][1] = params.transition_probs.deletion_from_match;
     for j in 2..(w.len() + 1) {
-        upper_prev[j] = upper_prev[j - 1] * params.transition_probs.deletion_from_deletion;
-        middle_prev[j] = 0.0;
+        forward_upper[0][j] = forward_upper[0][j - 1] * params.transition_probs.deletion_from_deletion;
     }
 
+    forward_lower[1][0] = params.transition_probs.insertion_from_match;
+    for i in 2..(v.len() + 1) {
+        forward_lower[i][0] = forward_lower[i-1][0] * params.transition_probs.insertion_from_insertion;
+    }
 
     let t = params.transition_probs;
     let e = params.emission_probs;
@@ -162,60 +164,43 @@ pub fn forward_algorithm_non_numerically_stable(
             w.len()
         };
 
-
-        if band_start == 1 {
-            upper_curr[0] = 0.0;
-            middle_curr[0] = 0.0;
-            if i == 1 {
-                lower_curr[0] = params.transition_probs.insertion_from_match
-            } else {
-                lower_curr[0] =
-                    lower_prev[0] * params.transition_probs.insertion_from_insertion;
-            }
-        }
-
-
         for j in band_start..(band_end + 1) {
-            let lower_continue = lower_prev[j] * t.insertion_from_insertion;
-            let lower_from_middle = middle_prev[j] * t.insertion_from_match;
-            lower_curr[j] = e.insertion * (lower_continue + lower_from_middle);
 
-            let upper_continue = upper_curr[j - 1] * t.deletion_from_deletion;
-            let upper_from_middle = middle_curr[j - 1] * t.deletion_from_match;
-            upper_curr[j] = e.deletion * (upper_continue + upper_from_middle);
+            last_hp[i][j] = if v[i - 1] == w[j - 1] {
+                if v[i - 1] == last_hp[i][j-1].0 {
+                    last_hp[i][j-1]
+                } else if w[j - 1] == last_hp[i-1][j].0 {
+                    last_hp[i-1][j]
+                } else {
+                    (v[i - 1], i, j)
+                }
+            } else {
+                ('N', i, j)
+            };
 
-            let middle_from_lower = lower_prev[j - 1] * t.match_from_insertion;
-            let middle_continue = middle_prev[j - 1] * t.match_from_match;
-            let middle_from_upper = upper_prev[j - 1] * t.match_from_deletion;
+            let lower_continue = forward_lower[i-1][j] * t.insertion_from_insertion;
+            let lower_from_middle = forward_middle[i-1][j] * t.insertion_from_match;
+            forward_lower[i][j] = e.insertion * (lower_continue + lower_from_middle);
+
+            let upper_continue = forward_upper[i][j - 1] * t.deletion_from_deletion;
+            let upper_from_middle = forward_middle[i][j - 1] * t.deletion_from_match;
+            forward_upper[i][j] = e.deletion * (upper_continue + upper_from_middle);
+
+            let middle_from_lower = forward_lower[i-1][j - 1] * t.match_from_insertion;
+            let middle_continue = forward_middle[i-1][j - 1] * t.match_from_match;
+            let middle_from_upper = forward_upper[i-1][j - 1] * t.match_from_deletion;
+
             let match_emission: f64 = if v[i - 1] == w[j - 1] {
                 e.equal
             } else {
                 e.not_equal
             };
-            middle_curr[j] =
+            forward_middle[i][j] =
                 match_emission * (middle_from_lower + middle_continue + middle_from_upper);
         }
-
-        for j in (band_start-1)..(band_end + 1) {
-            upper_prev[j] = upper_curr[j];
-            middle_prev[j] = middle_curr[j];
-            lower_prev[j] = lower_curr[j];
-        }
-        // we previously had a bug at the left boundary of the band... set these to NaN to make sure they
-        // aren't used again
-        if band_start >= 2 {
-            upper_prev[band_start-2] = f64::NAN;
-            middle_prev[band_start-2] = f64::NAN;
-            lower_prev[band_start-2] = f64::NAN;
-        }
-
-        upper_curr[band_start] = 0.0;
-        middle_curr[band_start] = 0.0;
-        lower_curr[band_start] = 0.0;
     }
 
-
-    LogProb::from(Prob(middle_prev[w.len()]))
+    LogProb::from(Prob(forward_middle[v.len()][w.len()]))
 
 }
 
